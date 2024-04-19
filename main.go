5b0005359734536b6d94fb577ef6916b812a50ae
Ashot Karapetian <AshotArtashesi@yandex.ru>
2024-04-19 10:05:39 +0300

    initial version

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io"
+	"os"
+	"os/exec"
 )
 
 func main() {
+	proj := flag.String("p", "", "Project directory.")
+	flag.Parse()
 
+	if err := run(*proj, os.Stdout); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
 
 func run(project string, out io.Writer) error {
@@ -14,5 +23,16 @@ func run(project string, out io.Writer) error {
 		return fmt.Errorf("project directory is required")
 	}
 
-	return nil
+	args := []string{"build", ".", "errors"}
+	cmd := exec.Command("go", args...)
+
+	cmd.Dir = project
+
+	if err := cmd.Run(); err != nil {
+		return fmt.Errorf("'go build' failed: %s", err)
+	}
+
+	_, err := fmt.Fprintln(out, "Go Build: SUCCESS")
+
+	return err
 }
